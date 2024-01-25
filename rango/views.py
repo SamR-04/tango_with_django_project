212ac111c89422857f3902a76f3a200a579d678e
Sam Russell <2883971r@student.gla.ac.uk>
2024-01-25 15:32:18 +0000

    chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,15 @@
+from rango.models import Category
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    # queries the category model to retreive the top 5 catagories , sortes in descending order  and nonly the friirst 5 
+
+    context_dict= {}
+    context_dict['noldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories']= category_list
+
     return render(request, 'rango/index.html', context = context_dict)
     #return HttpResponse ('Rango says hey there partner! <a href="/rango/about/">About</a>')
                          
